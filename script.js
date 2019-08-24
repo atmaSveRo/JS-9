66e145d4c4bb4d1e86d5f7220f39ad8e4f0aa472
atmaSveRo <romandronnikov@gmai.com>
2019-08-24 20:38:55 +0300

    вторая домашка

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-let money = 1;
-let income = 2;
-let addExpenses = 3;
-let deposit = 4;
-let mission = 5;
-let period = 6;
-console.log('deposit: ', deposit);
-alert(income);
+let money = 100000;
+income =  'вторая работа';
+addExpenses = ('ИПОТЕКА, КОММУНАЛКА, ПРОДУКТЫ');
+deposit = true;
+mission = 100000000000;
+period = 6;
+budgetDay = money/30;
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+console.log(income.length);
+console.log(period, mission);
+console.log(addExpenses.toLowerCase().split(', '));
+console.log(budgetDay, money%30);
+
+
+
+
 
