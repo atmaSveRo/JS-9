8fbe81e050ae560059990bd6e0734547bac6b221
atmaSveRo <romandronnikov@gmai.com>
2019-08-28 22:37:24 +0300

    допилил 4 домашку

--- script.js
+++ script.js
@@ -1,34 +1,31 @@
 'use strict';
 
-let money = +prompt('Укажите ваш месячный доход', 100000);
-let income = ('фриланс, такси, биржа');
-let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую', 'фриланс, такси, биржа');
-let deposit = confirm('Есть ли у вас депозит в банке?');
-let expens1 = prompt('Какие обязательные ежемесячные расходы у вас есть?', 'продукты');
-let cost1 = +prompt('Во сколько это обойдется?', 5000);
-let expens2 = prompt('Какие обязательные ежемесячные расходы у вас есть?', 'ипотека');
-let cost2 = +prompt('Во сколько это обойдется?', 10000);
-let budgetMonth = (money-cost1-cost2);
-let mission = 1000000;
-let period = Math.ceil((mission/budgetMonth)+'');
-let budgetDay = Math.floor(budgetMonth/30);
+let money = +prompt('Укажите ваш месячный доход', 100000),
+    income = ('фриланс, такси, биржа'),
+    addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую', 'фриланс, такси, биржа'),
+    deposit = confirm('Есть ли у вас депозит в банке?'),
+    expens1 = prompt('Какие обязательные ежемесячные расходы у вас есть?', 'продукты'),
+    cost1 = +prompt('Во сколько это обойдется?', 5000),
+    expens2 = prompt('Какие обязательные ежемесячные расходы у вас есть?', 'ипотека'),
+    cost2 = +prompt('Во сколько это обойдется?', 10000),
+    budgetMonth = (money-cost1-cost2),
+    mission = 1000000,
+    period = Math.ceil((mission/budgetMonth)+''),
+    budgetDay = Math.floor(budgetMonth/30);
 
 
 let ExpensesMonth = function getExpensesMonth() {
     return cost1 + cost2;
 };
 
-function getAccumulatedMonth() {
-    return money - cost1 - cost2;
+let accumulatedMonth = function getAccumulatedMonth() {
+    return money - ExpensesMonth();
 };
-console.log(getAccumulatedMonth());
-
-
-let accumulatedMonth = getAccumulatedMonth();
+console.log(accumulatedMonth());
 
 
 let TargetMonth = function getTargetMonth() {
-    return mission/accumulatedMonth;
+    return mission/accumulatedMonth();
 };
 console.log(Math.floor(TargetMonth()));
 
