437d20aa9c32ae048daaade4f9d894447d9adc18
atmaSveRo <romandronnikov@gmai.com>
2019-08-29 06:47:34 +0300

    пятая домашка

--- script.js
+++ script.js
@@ -1,22 +1,38 @@
 'use strict';
 
-let money = +prompt('Укажите ваш месячный доход', 100000),
+let money,
     income = ('фриланс, такси, биржа'),
     addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую', 'фриланс, такси, биржа'),
     deposit = confirm('Есть ли у вас депозит в банке?'),
     expens1 = prompt('Какие обязательные ежемесячные расходы у вас есть?', 'продукты'),
-    cost1 = +prompt('Во сколько это обойдется?', 5000),
+    // cost1 = +prompt('Во сколько это обойдется?', 5000),
     expens2 = prompt('Какие обязательные ежемесячные расходы у вас есть?', 'ипотека'),
-    cost2 = +prompt('Во сколько это обойдется?', 10000),
-    budgetMonth = (money-cost1-cost2),
-    mission = 1000000,
-    period = Math.ceil((mission/budgetMonth)+''),
-    budgetDay = Math.floor(budgetMonth/30);
+    // cost2 = +prompt('Во сколько это обойдется?', 10000),
+    // budgetMonth = (money-cost1-cost2),
+    mission = 1000000;
+    // period = Math.ceil((mission/budgetMonth)+''),
+    // budgetDay = Math.floor(budgetMonth/30);
+
+let start = function() {
+        
+    do {
+    money = prompt('Укажите ваш месячный доход', '');
+    }
+    while(isNaN(money) || money === ''|| money === null);
+    
+};
+    start();
 
 
 let ExpensesMonth = function getExpensesMonth() {
-    return cost1 + cost2;
+    let sum = 0;
+
+    for (let i = 0; i < 2; i++){
+        sum += +prompt('Во сколько это обойдется?', 5000)
+    }
+    return sum;
 };
+
 
 let accumulatedMonth = function getAccumulatedMonth() {
     return money - ExpensesMonth();
