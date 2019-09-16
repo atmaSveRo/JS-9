56f73ad8d025fa6b97e7e35b1927dbcd091832d9
atmaSveRo <romandronnikov@gmai.com>
2019-09-17 01:07:55 +0300

    доделал вторую часть обяз задания

--- script/NewYear/script.js
+++ script/NewYear/script.js
@@ -1,7 +1,7 @@
 window.addEventListener('DOMContentLoaded', function(){
     'use strict '
 
-    let date =  new Date('18 september 2019 21:00:00');
+    let date =  new Date();
     let date1 =Math.floor((Date.parse('31 december 2019') - Date.now())/1000/3600/24),
         time = date.toLocaleTimeString('en'),
         dayWeek = date.getDay('ru'),
@@ -17,14 +17,10 @@ window.addEventListener('DOMContentLoaded', function(){
             } else if (date.getHours() >= 12 && date.getHours() <= 17) {
                 document.write('Добрый день!'  + '<br \/>' +  'Сегодня: ' + arr[dayWeek]  + '<br \/>' +  'Текущее время: ' + time  + '<br \/>' +  'До Нового Года осталось: ' + date1 + ' дней')
 
-            } else if (date.getHours() > 17 && date.getHours() < 0 ) {
+            } else if (date.getHours() > 17 && date.getHours() < 24 ) {
                 document.write('Добрый вечер!'  + '<br \/>' +  'Сегодня: ' + arr[dayWeek]  + '<br \/>' +  'Текущее время: ' + time  + '<br \/>' +  'До Нового Года осталось: ' + date1 + ' дней')
             }
      }
             table();
-
-console.log('dayWeek: ', arr[dayWeek]);
-console.log('time: ', time);
-console.log('date: ', date1);
 
 });
