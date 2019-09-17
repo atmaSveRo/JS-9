968c6cfb96355bf360f2da7c1afd86aaa7f823e4
ROMAN DRONNIKOV <roman.dronnikov@eroglu.corp>
2019-09-17 12:16:27 +0300

    обновил дз и добавил комменты

--- script/script.js
+++ script/script.js
@@ -3,45 +3,48 @@ window.addEventListener('DOMContentLoaded', function(){
 
     // Timer
     function countTimer(deadline) {
+         // Объявляем переменные часов, минут, секунд
         let timerHours = document.querySelector('#timer-hours'),
              timerMinutes = document.querySelector('#timer-minutes'),
              timerSeconds = document.querySelector('#timer-seconds');
 
+          // задаём значения часам, минутам, секундам
         function getTimeRemaining() {
-             let dateStop = new Date(deadline).getTime(),
-                dateNow = new Date().getTime();
-                timeRemaining = (dateStop - dateNow) / 1000,
+             let dateStop = new Date(deadline).getTime(), // дата, от которой вычитаем в мс
+                dateNow = new Date().getTime();          // текущая дата в мс
+                timeRemaining = (dateStop - dateNow) / 1000, // разница в секундах
                 seconds = Math.floor(timeRemaining % 60),
                 minutes = Math.floor((timeRemaining / 60) % 60 ),
                 hours = Math.floor(timeRemaining / 3600);
                 return {timeRemaining, hours, minutes, seconds};      
              }
-
+          // останавливаем таймер при достижении нулевого значения
         function updateClock() {
-             console.log('ddd');
              
-            let timer = getTimeRemaining();
+            let timer = getTimeRemaining(); 
           
-            (timer.hours >= 10) ? timerHours.textContent = timer.hours : timerHours.textContent = '0' + timer.hours;
-            ( timer.minutes >= 10) ? timerMinutes.textContent = timer.minutes : timerMinutes.textContent = '0' + timer.minutes;
-            (timer.seconds >= 10) ? timerSeconds.textContent = timer.seconds : timerSeconds.textContent = '0' + timer.seconds;
+            (timer.hours >= 10) ? timerHours.textContent = timer.hours : timerHours.textContent = '0' + timer.hours; // добавляем 0 значениям меньше 10
+            ( timer.minutes >= 10) ? timerMinutes.textContent = timer.minutes : timerMinutes.textContent = '0' + timer.minutes; // добавляем 0 значениям меньше 10
+            (timer.seconds >= 10) ? timerSeconds.textContent = timer.seconds : timerSeconds.textContent = '0' + timer.seconds; // добавляем 0 значениям меньше 10
              
+            // высчитываем, достиг ли таймер нулевого значения
              if(timer.timeRemaining <= 0) {
-                 clearInterval(interval);
-                 timerHours.textContent = '00';
-                 timerMinutes.textContent ='00';
-                 timerSeconds.textContent = '00';
-                 timerHours.style.color = 'red';
-                 timerMinutes.style.color ='red'
-                 timerSeconds.style.color = 'red';
+                 clearInterval(interval); // останавливаем таймер
+                 timerHours.textContent = '00'; // приравниваем значение к нулю
+                 timerMinutes.textContent ='00'; // приравниваем значение к нулю
+                 timerSeconds.textContent = '00'; // приравниваем значение к нулю
+                 timerHours.style.color = 'red'; // раскрашиваем в красный
+                 timerMinutes.style.color ='red' // раскрашиваем в красный
+                 timerSeconds.style.color = 'red'; // раскрашиваем в красный
             }
 
         }
 
+        // объявляем переменную запуска таймера каждуб секунду
         let interval = setInterval(updateClock, 1000);
   };
 
- 
+     // устаналиваем дату отсчёта
      countTimer('18 september 2019');
 
 });
