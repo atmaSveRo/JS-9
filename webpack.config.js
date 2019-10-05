a15b331288e036f66ab93d406b46345f7193e512
atmaSveRo <romandronnikov@gmai.com>
2019-10-05 20:10:38 +0300

    добавлен файл webpack.config и gitignore

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+module.exports = {
+    entry: {
+        bundle: './src/index.js'
+    },
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: '[name].js',
+        publicPath: '/dist'
+    },
+    deServer: {
+        overlay: true
+    },
+};
