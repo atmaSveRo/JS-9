69025b402b74b400cfb959f1cabd33e9460ee4a9
atmaSveRo <romandronnikov@gmai.com>
2019-10-05 19:42:34 +0300

    создал модульную структуру

---
+++ src/index.js
@@ -0,0 +1,58 @@
+'use strict '
+
+import '@babel/polyfill';
+import 'nodelist-foreach-polyfill';
+import elementClosest from 'element-closest';
+elementClosest(window);
+
+import arrowUp from './modules/arrowUp';
+import burgerMenu from './modules/burgerMenu';
+import calc from './modules/calc';
+import carousel from './modules/carousel';
+import gift from './modules/gift';
+import slider from './modules/slider';
+import menu from './modules/menu';
+import modalWindow from './modules/modalWindow';
+import promoCode from './modules/promoCode';
+import sendForm from './modules/sendForm';
+
+
+//выпадающее меню
+
+menu();
+
+//модальные окна
+
+modalWindow();
+
+//Отправка форм в модальных окнах
+
+sendForm();
+
+//подарок
+
+gift();
+
+//слайдер
+
+slider();
+
+//карусель
+
+carousel();
+
+//калькулятор
+
+calc();
+
+//бургер меню
+
+burgerMenu();
+
+//появление стрелки
+
+arrowUp();
+
+//промокод
+
+promoCode();
