50fe4e93d915d67dff75409177bfb882c049897a
atmaSveRo <romandronnikov@gmai.com>
2019-10-10 13:37:05 +0300

    correct arrowUp func

--- src/modules/arrowUpModule.js
+++ src/modules/arrowUpModule.js
@@ -2,10 +2,10 @@ const arrowUpModule = () => {
 
 window.onscroll = function() {
   const scrolled = window.pageYOffset || document.documentElement.scrollTop;
-  if (scrolled > 741) {
-      document.getElementById('totop').style.display = 'block';
-  } else {
+  if (scrolled <= 741) {
       document.getElementById('totop').style.display = 'none';
+  } else {
+      document.getElementById('totop').style.display = 'block';
   }
 }
 
